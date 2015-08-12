89f22f6731bea4055c2c33875a5fa25fd6615afc
Ben Kogan <benkogan@me.com>
2015-08-12 17:11:11 -0400

    Move cabal config to .config/cabal

--- .config/fish/environment/env.fish
+++ .config/fish/environment/env.fish
@@ -5,8 +5,11 @@
 
 set -x EDITOR vim
 set -x TERM xterm-256color
+
 set -x XDG_CONFIG_HOME ~/.config
-set -x PYTHONSTARTUP ~/.config/python/startup.py
+set -x XDG_CACHE_HOME ~/.cache
+set -x PYTHONSTARTUP $XDG_CONFIG_HOME/python/startup.py
+set -x CABAL_CONFIG $XDG_CONFIG_HOME/cabal/config
 
 for dir in '~/bin' '~/.sandbox/.cabal-sandbox/bin' '/usr/texbin'
   if test -e $dir
