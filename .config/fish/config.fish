2364565d4431e9d2372ecea8bc6623485e2ca6c9
Ben Kogan <benkogan@me.com>
2015-08-12 11:32:31 -0400

    Switch use of `alias` for `abbr -a` in fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,29 +5,30 @@
 # Ben Kogan <http://benkogan.com>
 #
 
-. ~/.config/fish/environment/*
-
-alias c clear
-alias l 'ls -lah'
-alias cp 'cp -i'
-alias rm 'rm -i'
-alias mv 'mv -i'
-
-alias e $EDITOR
-alias vimrc 'e ~/.vimrc'
-alias fishrc 'e ~/.config/fish/config.fish'
-alias vimhelp 'mad ~/.help/vim-help.md'
-alias evimhelp 'e ~/.help/vim-help.md'
-
-alias gp 'git push'
-alias gl 'git log --color | less -r'
-alias gdh 'git diff --color HEAD | less -r'
-alias gst 'git status'
-alias gaa 'git add -A'
-alias gcm 'git commit -m'
+source ~/.config/fish/environment/*
 
 set -l j /usr/local/share/autojump/autojump.fish
-if test -f $j; . $j; end
+if test -f $j; source $j; end
+
+alias e $EDITOR
+
+abbr -a c  'clear'
+abbr -a l  'ls -lah'
+abbr -a cp 'cp -i'
+abbr -a rm 'rm -i'
+abbr -a mv 'mv -i'
+
+abbr -a gp  'git push'
+abbr -a gl  'git log --color | less -r'
+abbr -a gdh 'git diff --color HEAD | less -r'
+abbr -a gst 'git status'
+abbr -a gaa 'git add -A'
+abbr -a gcm 'git commit -m'
+abbr -a gco 'git checkout'
+abbr -a gcb 'git checkout -b'
+
+abbr -a vimrc  'e ~/.vimrc'
+abbr -a fishrc 'e ~/.config/fish/config.fish'
 
 function battery
   ioreg -n AppleSmartBattery -r | awk '$1~/Capacity/{c[$1]=$3} END{OFMT="%.2f%%"; max=c["\"MaxCapacity\""]; printf("%d%%", max>0? 100*c["\"CurrentCapacity\""]/max: "?")}'
