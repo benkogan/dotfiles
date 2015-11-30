f2fb099c1c582c283f7babdaccbeb1cc88e6be9f
Ben Kogan <benkogan@me.com>
2015-11-30 17:07:16 -0500

    Add more git abbreviations, remove more cabal baggage

--- .config/fish/environment/env.fish
+++ .config/fish/environment/env.fish
@@ -9,9 +9,8 @@ set -x TERM xterm-256color
 set -x XDG_CONFIG_HOME ~/.config
 set -x XDG_CACHE_HOME ~/.cache
 set -x PYTHONSTARTUP $XDG_CONFIG_HOME/python/startup.py
-set -x CABAL_CONFIG $XDG_CONFIG_HOME/cabal/config
 
-for dir in '~/bin' '~/.sandbox/.cabal-sandbox/bin' '/usr/texbin'
+for dir in '~/bin' '/usr/texbin'
   if test -e $dir
     set -x fish_user_paths $fish_user_paths $dir
   end
