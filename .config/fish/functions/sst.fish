a91c3991716ca3afb46a0dd2afb00aa84f61e5fe
Ben Kogan <benkogan@me.com>
2017-03-03 16:21:46 -0500

    Fix sst fn

--- .config/fish/functions/sst.fish
+++ .config/fish/functions/sst.fish
@@ -1,4 +1,4 @@
 function sst -d "SSH into given box and start or attach to tmux."
   set --local session_name "persistent"
-  ssh -t $argv "tmux attach || tmux new $session_name"
+  ssh -t $argv "tmux attach || tmux new -s $session_name"
 end
