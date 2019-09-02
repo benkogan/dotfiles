00d32776b9b0360d75c93f9ad8d9b6f97fa7cdf0
Ben Kogan <benjamink@spotify.com>
2019-09-02 14:41:28 -0400

    Move autojump setup from config.fish to environment script

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,6 +4,3 @@ for f in ~/.config/fish/environment/*.fish
   source $f
 end
 
-set -l j /usr/local/share/autojump/autojump.fish
-if test -f $j; source $j; end
-
---
+++ .config/fish/environment/autojump.fish
@@ -0,0 +1,5 @@
+set -l j /usr/local/share/autojump/autojump.fish
+
+if test -f $j
+  source $j
+end
