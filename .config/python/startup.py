edecbdea618f8aa68da404608d5aaa26ea20ea42
Ben Kogan <benjamink@spotify.com>
2019-09-02 14:32:53 -0400

    Make python/startup.py failure-tolerant

--- .config/python/startup.py
+++ .config/python/startup.py
@@ -1,8 +1,16 @@
 # from <http://stackoverflow.com/questions/7116038>
 
-import readline
-import rlcompleter
-if 'libedit' in readline.__doc__:
-    readline.parse_and_bind("bind ^I rl_complete")
-else:
-    readline.parse_and_bind("tab: complete")
+error_message = """
+\033[91mError:\033[0m unable to import readline bindings.
+You may need to `pip install readline rlcompleter`."""
+
+try:
+    import readline
+    import rlcompleter
+    if 'libedit' in readline.__doc__:
+        readline.parse_and_bind("bind ^I rl_complete")
+    else:
+        readline.parse_and_bind("tab: complete")
+except ImportError as e:
+    print(error_message)
+    print(e)
